68b469daa1f6dad189c92a6c16d7b3a317945791
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-04 18:06:41 +0530

    Try not re-installing

--- src/components/Layer0.jsx
+++ src/components/Layer0.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from 'react'
 import install from '@layer0/prefetch/window/install'
 
-export default function Layer0({}) {
+export default function Layer0({ }) {
   useEffect(() => {
-    install()
+    navigator.serviceWorker.getRegistrations().then((t) => {
+      console.log(t)
+      if (t.length < 1) {
+        install()
+      }
+    })
   }, [])
   return <></>
 }
