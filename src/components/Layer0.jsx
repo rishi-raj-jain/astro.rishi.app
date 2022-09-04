646d4c54373aba8eac67f00d3eb68136ffe7f47e
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-04 17:21:47 +0530

    Prefetch dark and light css

---
+++ src/components/Layer0.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import install from '@layer0/prefetch/window/install'
+
+export default function Layer0({ }) {
+    useEffect(() => {
+        install()
+    }, [])
+    return <></>
+}
