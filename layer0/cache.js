006ebcb0ae055fc4d0903420d6f6baf56f16b7e2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-09-04 21:19:29 +0530

    Update cache.js

--- layer0/cache.js
+++ layer0/cache.js
@@ -1,7 +1,7 @@
 export const ONE_DAY_CACHE_HANDLER = ({ cache }) => {
   cache({
     edge: {
-      maxAgeSeconds: 60 * 60,
+      maxAgeSeconds: 60 * 60 * 24,
       // Cache responses even if they contain cache-control: private header
       // https://docs.layer0.co/guides/caching#private
       // https://docs.layer0.co/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
