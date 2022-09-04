f9da722ea7ea611d0edb627054e1ad788e7dec63
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-04 16:56:39 +0530

    DeepFetch

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,9 +1,47 @@
-import { Prefetcher } from '@layer0/prefetch/sw'
-import { precacheAndRoute } from 'workbox-precaching'
 import { skipWaiting, clientsClaim } from 'workbox-core'
+import { Prefetcher, prefetch } from '@layer0/prefetch/sw'
+import DeepFetchPlugin from '@layer0/prefetch/sw/DeepFetchPlugin'
 
 skipWaiting()
 clientsClaim()
-precacheAndRoute(self.__WB_MANIFEST || [])
 
-new Prefetcher().route()
+new Prefetcher({
+  plugins: [
+    new DeepFetchPlugin([
+      {
+        selector: 'script',
+        maxMatches: 20,
+        attribute: 'src',
+        as: 'script',
+        callback: deepFetchAssets,
+      },
+      {
+        selector: '[rel="stylesheet"]',
+        maxMatches: 20,
+        attribute: 'href',
+        as: 'style',
+        callback: deepFetchAssets,
+      },
+      {
+        selector: '[rel="preload"]',
+        maxMatches: 20,
+        attribute: 'href',
+        as: 'style',
+        callback: deepFetchAssets,
+      },
+    ]),
+  ],
+}).route()
+  .cache(/^https:\/\/(.*?)\.com\/.*/)
+  .cache(/^https:\/\/(.*?)\.net\/.*/)
+
+function deepFetchAssets({ $el, el, $ }) {
+  let urlTemplate = $(el).attr('href')
+  if (urlTemplate) {
+    prefetch(urlTemplate)
+  }
+  urlTemplate = $(el).attr('src')
+  if (urlTemplate) {
+    prefetch(urlTemplate)
+  }
+}
