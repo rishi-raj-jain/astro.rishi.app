73a0ca367d0fcddf1f2743fcf94485a2eb8aed80
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-08 10:53:27 +0530

    Measure + Promises to keep my code in promise

--- layer0/transformResponse.js
+++ layer0/transformResponse.js
@@ -8,100 +8,92 @@ import esImport from '@layer0/core/utils/esImport'
 
 export default async function transformResponse(res, req) {
   let resOriginal = res.body
-  const longTask = () =>
-    new Promise(async (resolve) => {
-      // Else, try this code which does some good performance optimization
-      try {
-        // If it's an HTML, then add the prefetch tags from the JSON
-        if (res.getHeader('content-type') === 'text/html') {
-          const $ = cheerio.load(res.body)
 
-          // Read the list of URLs to be prefetched
-          const prefetchList = JSON.parse(fs.readFileSync('./toPrefetchList.json', 'utf8'))
+  try {
+    const promise1 = new Promise(async (resolve, reject) => {
+      if (res.getHeader('content-type') === 'text/html') {
+        const $ = cheerio.load(res.body)
 
-          // Create prefetch urls that will be prefetched by SW
-          prefetchList.prefetch.forEach((i) => $('body').append(`<prefetch-url url="/${i}" />`))
+        // Read the list of URLs to be prefetched
+        const prefetchList = JSON.parse(fs.readFileSync('./toPrefetchList.json', 'utf8'))
 
-          // Find the stylesheet that calls the global CSS -> a css that contains styles for the whole app -> created by astro
-          const cssHrefSelector = $(`link[rel="stylesheet"][href*="about-"][href*=".css"]`)
+        // Create prefetch urls that will be prefetched by SW
+        prefetchList.prefetch.forEach((i) => $('body').append(`<prefetch-url url="/${i}" />`))
 
-          // If such a selector exists, remove the existing link and generate CSS
-          if (cssHrefSelector && cssHrefSelector.attr('href').length > 0) {
-            // Define the base CSS, use this as the global styles
-            const sourceCSS = `
-          @tailwind base;
-          @tailwind components;
-          @tailwind utilities;
-        `
+        // Find the stylesheet that calls the global CSS -> a css that contains styles for the whole app -> created by astro
+        const cssHrefSelector = $(`link[rel="stylesheet"][href*="about-"][href*=".css"]`)
 
-            // Remove the sets of content to purge as nothing to purge there
-            delete baseConfig['content']
+        // If such a selector exists, remove the existing link and generate CSS
+        if (cssHrefSelector && cssHrefSelector.attr('href').length > 0) {
+          // Define the base CSS, use this as the global styles
+          const sourceCSS = `
+            @tailwind base;
+            @tailwind components;
+            @tailwind utilities;
+          `
 
-            const tailwindConfig = {
-              // Pass the plugin preset
-              presets: [baseConfig],
-              // Defining content as the html of the page to just generate the CSS received on the page
-              content: [{ raw: $.html(), extension: 'html' }],
-            }
+          // Remove the sets of content to purge as nothing to purge there
+          delete baseConfig['content']
 
-            // Get the processsed css
-            const css = await postcss([tailwindcss(tailwindConfig)]).process(sourceCSS)
-
-            // If there's a css, append it to the page
-            if (css.css.length > 0) {
-              $('head').append(`<style>${css.css}</style>`)
-              // Remove such element if it exists
-              // This is blocking CSS and what I attempt to do is to create CSS on the fly instead
-              // Why? Blocking CSS it should be but not require the CSS of the whole app to be blocking
-              $(`link[rel="stylesheet"][href*="about-"][href*=".css"]`).remove()
-            }
-          } else {
-            console.log('Did not found such a selector.')
+          const tailwindConfig = {
+            // Pass the plugin preset
+            presets: [baseConfig],
+            // Defining content as the html of the page to just generate the CSS received on the page
+            content: [{ raw: $.html(), extension: 'html' }],
           }
 
-          // ES-Import html-minifier es module
-          const { minify } = await esImport('html-minifier')
+          // Get the processsed css
+          const css = await postcss([tailwindcss(tailwindConfig)]).process(sourceCSS)
 
-          // Replace the a.storyblok.com path with the proxied path
-          res.body = minify($.html().replace(/https:\/\/a\.storyblok\.com\//g, '/l0-storyblok/'), {
-            decodeEntities: true,
-            minifyCSS: true,
-            minifyJS: true,
-            processConditionalComments: true,
-            removeEmptyAttributes: true,
-            removeRedundantAttributes: true,
-            trimCustomFragments: true,
-            useShortDoctype: true,
-            // collapseWhitespace: true, // Can't do this as it leads to client-side react errors
-            collapseInlineTagWhitespace: true,
-          })
-          resolve()
-          // .replace(/>\s+</g, '><') // Can't do this as this affects how code is rendered
+          // If there's a css, append it to the page
+          if (css.css.length > 0) {
+            $('head').append(`<style>${css.css}</style>`)
+            // Remove such element if it exists
+            // This is blocking CSS and what I attempt to do is to create CSS on the fly instead
+            // Why? Blocking CSS it should be but not require the CSS of the whole app to be blocking
+            $(`link[rel="stylesheet"][href*="about-"][href*=".css"]`).remove()
+          }
+        } else {
+          console.log('Did not found such a selector.')
         }
-      } catch (e) {
-        // Preserve the initial response body in case of failure
-        res.body = resOriginal
-        res.statusCode = 200
-        console.log(e)
-        resolve()
+
+        // ES-Import html-minifier es module
+        const { minify } = await esImport('html-minifier')
+
+        // Replace the a.storyblok.com path with the proxied path
+        res.body = minify($.html().replace(/https:\/\/a\.storyblok\.com\//g, '/l0-storyblok/'), {
+          decodeEntities: true,
+          minifyCSS: true,
+          minifyJS: true,
+          processConditionalComments: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true,
+          trimCustomFragments: true,
+          useShortDoctype: true,
+          // collapseWhitespace: true, // Can't do this as it leads to client-side react errors
+          collapseInlineTagWhitespace: true,
+        })
+        await resolve(res.body)
+        // .replace(/>\s+</g, '><') // Can't do this as this affects how code is rendered
+      } else {
+        await resolve(resOriginal)
       }
     })
-  const timeout = (cb, interval) => () => new Promise((resolve) => setTimeout(() => cb(resolve), interval))
-  const onTimeout = timeout(async (resolve) => {
-    res.body = resOriginal
-    console.log('[10s]', req.url)
-    resolve()
-  }, 10 * 1000)
-  try {
+
+    const promise2 = new Promise((resolve, reject) => {
+      setTimeout(resolve, 10 * 1000, resOriginal)
+    })
+
     var startTime = performance.now()
-    // Regardless of what happens, return original body after 10 seconds
-    await Promise.race([longTask, onTimeout])
+
+    const decideBody = await Promise.race([promise1, promise2])
+
     var endTime = performance.now()
+
     console.log(`Call to ${req.url} took ${endTime - startTime} milliseconds`)
+    res.body = decideBody
   } catch (e) {
-    // Preserve the initial response body in case of failure
     res.body = resOriginal
-    res.statusCode = 200
-    console.log(e)
   }
+  return
 }
