633703b5ad91b6bd7ced8417288e0e16f5073442
Rishi Raj Jain <rjain@llnw.com>
2022-12-11 20:37:34 +0530

    Update html-minifier deps

--- edgio.config.js
+++ edgio.config.js
@@ -7,5 +7,12 @@ module.exports = {
   },
   includeFiles: {
     'node_modules/html-minifier/**/*': true,
+    'node_modules/camel-case/**/*': true,
+    'node_modules/clean-css/**/*': true,
+    'node_modules/commander/**/*': true,
+    'node_modules/he/**/*': true,
+    'node_modules/relateurl/**/*': true,
+    'node_modules/uglify-js/**/*': true,
+    'node_modules/param-case/**/*': true,
   },
 }
