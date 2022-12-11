3ffe1c4fa5aa3f25e96519585e19102faddc28da
Rishi Raj Jain <rjain@llnw.com>
2022-12-11 18:53:49 +0530

    Minify HTML

--- routes.js
+++ routes.js
@@ -1,14 +1,17 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
+import { load } from 'cheerio'
 import { Router } from '@edgio/core'
+import { minify } from 'html-minifier'
 import { astroRoutes } from '@edgio/astro'
+import { minifyOptions } from 'minifyOptions'
 
 const paths = ['/', '/cv', '/blogs', '/storyblok', '/about', '/blog/:path*']
 
 const router = new Router()
 
 paths.forEach((i) => {
-  router.match(i, ({ cache, removeUpstreamResponseHeader }) => {
+  router.match(i, ({ cache, removeUpstreamResponseHeader, renderWithApp }) => {
     removeUpstreamResponseHeader('cache-control')
     cache({
       edge: {
@@ -16,6 +19,12 @@ paths.forEach((i) => {
       },
       browser: false,
     })
+  })
+  renderWithApp({
+    transformResponse: (res, req) => {
+      const $ = load(res.body)
+      res.body = minify($.html(), minifyOptions)
+    },
   })
 })
 
