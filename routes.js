b1a2b852f29963095d34ffe7ae595e2221b3aa30
Rishi Raj Jain <rjain@llnw.com>
2023-03-28 12:06:09 +0530

    Simplify

--- routes.js
+++ routes.js
@@ -1,50 +1,12 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
 
-import { load } from 'cheerio'
-import fetch from 'node-fetch'
 import { astroRoutes } from '@edgio/astro'
-import { minifyOptions } from 'minifyOptions'
-import esImport from '@edgio/core/utils/esImport'
-import { getAllPostsForHome } from '@/src/lib/api'
 import { CustomCacheKey, Router } from '@edgio/core'
-import { isProductionBuild } from '@edgio/core/environment'
 
 const paths = ['/', '/cv', '/blogs', '/storyblok', '/about', '/blog/:path', '/showcase/:path']
 
-const router = new Router({ indexPermalink: true })
-
-router.prerender(async () => {
-  const blogs = await getAllPostsForHome()
-  const nonDynamicPaths = ['/', '/cv', '/about', '/blogs', '/storyblok']
-  const paths = [...blogs.map((i) => ({ path: `/blog/${i.slug}` })), ...nonDynamicPaths.map((i) => ({ path: i }))]
-  paths.forEach((i) => {
-    fetch(`${process.env.DOMAIN_URL}${i.path}`)
-  })
-  if (paths.length > 0 && process.env.DOMAIN_URL && process.env.SPEED_VITALS_KEY) {
-    const resp = await fetch('https://ttfb-booster.fly.dev', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        paths,
-        baseURL: process.env.DOMAIN_URL,
-        SPEED_VITALS_KEY: process.env.SPEED_VITALS_KEY,
-      }),
-    })
-    if (resp.ok) {
-      const data = await resp.json()
-      console.log(JSON.stringify({ data }))
-    }
-  }
-  return paths
-})
-
-// Route requests requesting showcase assets to showcase prefix
-router.match('/showcase/:path*.:ext(js|mjs|css|png|ico|svg|jpg|jpeg|gif|ttf|woff|otf|mp4)', ({ serveStatic }) => {
-  serveStatic('dist/client/showcase/:path*.:ext')
-})
+const router = new Router()
 
 // Disable cross origin fetch of /api route
 router.match('/api/:path*', ({ setResponseHeader }) => {
@@ -52,30 +14,14 @@ router.match('/api/:path*', ({ setResponseHeader }) => {
 })
 
 paths.forEach((i) => {
-  router.match(i, ({ cache, removeUpstreamResponseHeader, renderWithApp }) => {
+  router.match(i, ({ cache, removeUpstreamResponseHeader }) => {
     removeUpstreamResponseHeader('cache-control')
     cache({
       edge: {
         maxAgeSeconds: 60,
         staleWhileRevalidateSeconds: 60 * 60 * 24 * 365,
       },
-      browser: false,
       key: new CustomCacheKey().excludeAllQueryParameters(),
-    })
-    renderWithApp({
-      transformResponse: async (res, req) => {
-        let statusCodeOriginal = res.statusCode
-        try {
-          if (res.getHeader('content-type').includes('html')) {
-            const $ = load(res.body)
-            const { minify } = await esImport('html-minifier')
-            res.body = minify($.html(), minifyOptions)
-          }
-        } catch (e) {
-          console.log(e.message || e.toString())
-          res.statusCode = statusCodeOriginal
-        }
-      },
     })
   })
 })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,6 @@ const noTailwindCode = {
   'code::after': false,
 }
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
