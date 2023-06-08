48991c3134652b06458205277959d66304593b8c
Rishi Raj Jain <rjain@llnw.com>
2023-06-08 21:40:53 +0530

    remove redundant code

--- edgio/build.js
+++ edgio/build.js
@@ -2,13 +2,10 @@ const { join } = require('path')
 const { nodeFileTrace } = require('@vercel/nft')
 const { DeploymentBuilder } = require('@edgio/core/deploy')
 
-const appDir = process.cwd()
-
 module.exports = async function build() {
   const builder = new DeploymentBuilder()
   builder.clearPreviousBuildOutput()
   await builder.exec('npx astro build')
-  builder.addJSAsset(join(appDir, '.next', 'standalone'), 'dist')
   await builder.build()
   const { fileList } = await nodeFileTrace(['./dist/server/entry.mjs'])
   fileList.forEach((file) => builder.copySync(file, join(builder.jsDir, file)))
