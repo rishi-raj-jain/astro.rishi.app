0fe3eebfd725152b52b6259291f07384212ca8e7
Rishi Raj Jain <rjain@llnw.com>
2023-06-09 05:00:22 +0530

    Update homepage

--- edgio/build.js
+++ edgio/build.js
@@ -8,5 +8,6 @@ module.exports = async function build() {
   await builder.exec('npx astro build')
   await builder.build()
   const { fileList } = await nodeFileTrace(['./dist/server/entry.mjs'])
+  builder.addJSAsset(join(process.cwd(), 'public', 'static', 'favicon-image.jpg'))
   fileList.forEach((file) => builder.copySync(file, join(builder.jsDir, file)))
 }
--- src/lib/image.js
+++ src/lib/image.js
@@ -3,9 +3,9 @@ import imagemin from 'imagemin'
 import imageminJpegtran from 'imagemin-jpegtran'
 
 export async function getBase64ImageUrl(imagePath) {
-  const buffer = fs.readFileSync(imagePath)
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
-    plugins: [imageminJpegtran({ progressive: true })],
+  const buffer = Buffer.from(fs.readFileSync(imagePath))
+  const minified = await imagemin.buffer(buffer, {
+    plugins: [imageminJpegtran()],
   })
   const base64 = Buffer.from(minified).toString('base64')
   return `data:image/jpeg;base64,${base64}`
