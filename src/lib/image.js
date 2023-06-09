999105b3ff6daa2780774eb338d23618563e64ed
Rishi Raj Jain <rjain@llnw.com>
2023-06-09 15:49:14 +0530

    Update home page

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,12 +1,16 @@
 import fs from 'fs'
 import imagemin from 'imagemin'
+import { imageSize } from 'image-size'
+import resizeImg from 'resize-image-buffer'
 import imageminJpegtran from 'imagemin-jpegtran'
 
 export async function getBase64ImageUrl(imagePath) {
   const buffer = Buffer.from(fs.readFileSync(imagePath))
-  const minified = await imagemin.buffer(buffer, {
+  const { width, height } = imageSize(buffer)
+  const resizedImage = await resizeImg(buffer, { width: 10, height: (height * 10) / width })
+  const minified = await imagemin.buffer(resizedImage, {
     plugins: [imageminJpegtran()],
   })
   const base64 = Buffer.from(minified).toString('base64')
-  return `data:image/jpeg;base64,${base64}`
+  return { width, height, data: `data:image/jpeg;base64,${base64}` }
 }
