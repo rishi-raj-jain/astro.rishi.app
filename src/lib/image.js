0022f6f1e32bb1e92cdab0793a6db077b1773394
Rishi Raj Jain <rjain@llnw.com>
2023-06-09 16:58:12 +0530

    Update blogs page

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,11 +1,19 @@
 import fs from 'fs'
+import fetch from 'node-fetch'
 import imagemin from 'imagemin'
 import { imageSize } from 'image-size'
 import resizeImg from 'resize-image-buffer'
 import imageminJpegtran from 'imagemin-jpegtran'
 
 export async function getBase64ImageUrl(imagePath) {
-  const buffer = Buffer.from(fs.readFileSync(imagePath))
+  let buffer
+  if (!imagePath) return
+  if (imagePath.startsWith('https') || imagePath.startsWith('//')) {
+    const response = await fetch(imagePath.replace('https://', '//').replace('//', 'https://'))
+    buffer = Buffer.from(await response.arrayBuffer())
+  } else {
+    buffer = Buffer.from(fs.readFileSync(imagePath))
+  }
   const { width, height } = imageSize(buffer)
   const resizedImage = await resizeImg(buffer, { width: 10, height: (height * 10) / width })
   const minified = await imagemin.buffer(resizedImage, {
